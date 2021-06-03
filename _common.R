1236316d925d7865b03f17d30b6202843956f875
Enrico Spinielli <enrico.spinielli@gmail.com>
2021-06-03 11:53:59 +0200

    new config

---
+++ _common.R
@@ -0,0 +1,15 @@
+set.seed(8765)
+options(digits = 3)
+
+knitr::opts_chunk$set(
+  echo      = FALSE,
+  message   = FALSE,
+  warning   = FALSE,
+  cache     = FALSE,
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp   = 0.618,  # 1 / phi
+  fig.show  = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
